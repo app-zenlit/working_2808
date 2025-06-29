bca27d551e7b796f6e42e7788faa2137a807a197
Arjun Gowda L <arjungowdal4601@gmail.com>
2025-06-29 23:13:13 +0530

    Change 'Nearby Feed' to 'Feed' in HomeScreen header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -247,7 +247,7 @@ export const HomeScreen: React.FC<Props> = ({
             <svg className="w-8 h-8 mr-3 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
             </svg>
-            <h1 className="text-xl font-bold text-white">Nearby Feed</h1>
+            <h1 className="text-xl font-bold text-white">Feed</h1>
           </div>
           
           {/* Top Right Icons */}
