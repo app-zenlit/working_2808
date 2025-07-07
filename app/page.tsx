090d65b12776fd9987d5260c8ac248fd54bb9927
Arjun Gowda L <arjungowdal4601@gmail.com>
2025-07-07 22:22:34 +0530

    Fix Client Component Event Handler Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { redirect } from 'next/navigation'
 import { Button } from '../components/ui/button'
 import { SparklesIcon, ArrowDownIcon, MapPinIcon, UserGroupIcon, ChatBubbleLeftIcon, CheckCircleIcon, EyeSlashIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
